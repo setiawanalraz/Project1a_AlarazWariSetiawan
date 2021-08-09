bd05d08be6aa458c0c25f4cb836254794a8ce8b6
setiawanalraz <alrazsetiawan79@gmail.com>
2021-08-09 10:27:49 +0700

    Penambahan fitur menampilkan nama depan dan nama belakang

--- app/src/main/java/vsga/mobile/project1a_alarazwarisetiawan/MainActivity.java
+++ app/src/main/java/vsga/mobile/project1a_alarazwarisetiawan/MainActivity.java
@@ -3,12 +3,35 @@ package vsga.mobile.project1a_alarazwarisetiawan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edtNamaDepan, edtNamaBelakang;
+    Button btnTampilkan;
+    TextView tvHasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtNamaDepan = (EditText) findViewById(R.id.edt_namaDepan);
+        edtNamaBelakang = (EditText) findViewById(R.id.edt_namaBelakang);
+        btnTampilkan = (Button) findViewById(R.id.btn_tampilkan);
+        tvHasil = (TextView) findViewById(R.id.tv_hasil);
+
+        btnTampilkan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nama_depan = edtNamaDepan.getText().toString();
+                String nama_belakang = edtNamaBelakang.getText().toString();
+                String hasil = nama_depan + " " + nama_belakang;
+
+                tvHasil.setText(hasil);
+            }
+        });
     }
 }
